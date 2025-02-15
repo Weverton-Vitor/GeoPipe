8904418719b78d692d9a4d8711f1df132dfda8db
Weverton Vitor <wevertonvitor01@gmail.com>
2025-02-15 07:21:05 -0300

    feat(pipeline): new pipelines

--- src/fmask_pipeline/pipelines/download/pipeline.py
+++ src/fmask_pipeline/pipelines/download/pipeline.py
@@ -6,8 +6,6 @@ generated using Kedro 0.19.10
 from kedro.pipeline import Pipeline, node, pipeline
 
 from .nodes import (
-    apply_fmask,
-    cloud_removal,
     create_dirs,
     donwload_images,
     shapefile2feature_collection,
