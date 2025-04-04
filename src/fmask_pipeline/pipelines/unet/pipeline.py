0cbff256f70d0b8009a8ca0181935b0d379dfb5c
Arthur César <arthurcesat@gmail.com>
2025-04-04 14:48:46 -0300

    feat(unet): enhance UNet model loading and inference; add cloud removal functionality and remove unused utils

--- src/fmask_pipeline/pipelines/unet/pipeline.py
+++ src/fmask_pipeline/pipelines/unet/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.19.12
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
-from .nodes import apply_unet
+from .nodes import apply_unet, cloud_removal
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
@@ -17,10 +17,27 @@ def create_pipeline(**kwargs) -> Pipeline:
                     "save_masks_path": "params:configs.save_masks_path",
                     "save_plots_path": "params:configs.save_plot_masks_path",
                     "skip_masks": "params:configs.skip_masks",
-                    "unet_params": "params:configs.unet",  # parâmetros específicos do Unet
+                    "unet_params": "params:configs.unet",  
                 },
                 outputs="unet_segmentation_output",
                 name="apply_UNet",
-            )
+            ),
+            node(
+                func=cloud_removal,
+                inputs={
+                    "dependency": "dependency4",
+                    "path_images": "params:configs.boa_dowload_path",
+                    "path_masks": "params:configs.save_masks_path",
+                    "output_path": "params:configs.save_clean_images_path",
+                    "location_name": "params:configs.location_name",
+                    "cloud_and_cloud_shadow_pixels": "params:configs.cloud_and_cloud_shadow_pixels",
+                    "init_date": "params:configs.init_date",
+                    "final_date": "params:configs.final_date",
+                    "skip_clean": "params:configs.skip_clean",
+                    "color_file_log_path": "params:configs.cloud_removal_log",
+                },
+                outputs="dependency5",
+                name="Cloud_removal",
+            ),
         ]
     )
